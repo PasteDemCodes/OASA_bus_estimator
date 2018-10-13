35ca5c4837c7e49d97793861c10908ff6bc260e8
Dimitrios Zografakis <zog.dim3@gmail.com>
2018-10-13 19:08:02 +0300

    v0.2 working!

--- oasa.py
+++ oasa.py
@@ -1,7 +1,8 @@
 from requests import get
 from bs4 import BeautifulSoup
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+import time
 
 station_id = '280066'
 station_name = 'ΓΑΖΙΑΣ'
@@ -10,14 +11,37 @@ url = 'http://telematics.oasa.gr/en/#stationInfo_' + station_id + '_' + station_
 
 # Download latest chromedriver here: -> http://chromedriver.chromium.org/downloads.
 # Then add to path.
-driver = webdriver.Chrome()
+
+# Configuring chrome to start headless
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+driver = webdriver.Chrome(chrome_options=options)
+
+
+print()
+print('Chromedriver Started')
+
 driver.get(url)
 
-session = get(url)
+print('Results Fetched')
+
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-bus_arrivals = soup.find_all('ul', class_ = 'list-group tArrivals')
+bus_arrivals = soup.find_all('ul', class_='list-group tArrivals')
 
-for bus in bus_arrivals:
-    print(bus)
+bus_names = []
+bus_arrivals = []
+
+for cl in soup.find_all('ul', class_='list-group tArrivals'):
+    for but in cl.find_all('button'):
+        bus_names.append(but.text)
+    for time in cl.find_all('span'):
+        bus_arrivals.append(time.text)
+
+
+for counter, bus in enumerate(bus_names):
+    print('%s estimated arrival in: %s' % (bus, bus_arrivals[counter]))
+
